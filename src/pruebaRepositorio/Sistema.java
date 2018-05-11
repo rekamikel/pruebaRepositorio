1c258a680a9324d421245603588a93573d566f67
usuario <usuario@asus>
2018-05-11 20:51:47 +0200

    commit 2. un super for añadido

--- src/pruebaRepositorio/Sistema.java
+++ src/pruebaRepositorio/Sistema.java
@@ -4,6 +4,10 @@ public class Sistema {
 
 	public static void main(String[] args) {
 		System.out.println("pruebaRepositorio");
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+			
+		}
 
 	}
 
