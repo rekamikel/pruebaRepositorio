30e7e5e08effc469faabf616f5fe085572ff35a2
usuario <usuario@asus>
2018-05-11 20:41:10 +0200

    Commit 1 . Variación del mesaje por consola

--- src/pruebaRepositorio/Sistema.java
+++ src/pruebaRepositorio/Sistema.java
@@ -3,7 +3,7 @@ package pruebaRepositorio;
 public class Sistema {
 
 	public static void main(String[] args) {
-		System.out.println("pruebaRepositorio");
+		System.out.println("pruebaRepositorioNuevaRama, llena de oportunidades para crear");
 
 	}
 
